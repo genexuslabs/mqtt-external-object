4f66b138570023488ced11efd9ac3577939b26b1
Seba Gómez <sebagomezcorrea@outlook.com>
2020-04-20 18:04:07 -0300

    Format document

--- src/MQTTLib/ConsoleLogger.cs
+++ src/MQTTLib/ConsoleLogger.cs
@@ -3,28 +3,28 @@ using MQTTnet.Diagnostics;
 
 namespace MQTTLib
 {
-    class ConsoleLogger : IMqttNetLogger
-    {
-        public event EventHandler<MqttNetLogMessagePublishedEventArgs> LogMessagePublished;
+	class ConsoleLogger : IMqttNetLogger
+	{
+		public event EventHandler<MqttNetLogMessagePublishedEventArgs> LogMessagePublished;
 
-        public ConsoleLogger()
-        {
-            LogMessagePublished += ConsoleLogger_LogMessagePublished;
-        }
+		public ConsoleLogger()
+		{
+			LogMessagePublished += ConsoleLogger_LogMessagePublished;
+		}
 
-        private void ConsoleLogger_LogMessagePublished(object sender, MqttNetLogMessagePublishedEventArgs e) => Console.WriteLine($"({e.LogMessage.Source}){e.LogMessage.Level}:{e.LogMessage.Message}");
+		private void ConsoleLogger_LogMessagePublished(object sender, MqttNetLogMessagePublishedEventArgs e) => Console.WriteLine($"({e.LogMessage.Source}){e.LogMessage.Level}:{e.LogMessage.Message}");
 
-        public IMqttNetLogger CreateChildLogger(string source)
-        {
-            Console.WriteLine($"CreateChildLogger:{source}");
-            return new ConsoleLogger();
-        }
+		public IMqttNetLogger CreateChildLogger(string source)
+		{
+			Console.WriteLine($"CreateChildLogger:{source}");
+			return new ConsoleLogger();
+		}
 
-        public void Publish(MqttNetLogLevel logLevel, string message, object[] parameters, Exception exception)
-        {
-            Console.WriteLine($"Publish:{logLevel},'{string.Format(message, parameters)}'");
-            if (exception != null)
-                Console.WriteLine($"Publish ERROR: {exception.Message}");
-        }
-    }
+		public void Publish(MqttNetLogLevel logLevel, string message, object[] parameters, Exception exception)
+		{
+			Console.WriteLine($"Publish:{logLevel},'{string.Format(message, parameters)}'");
+			if (exception != null)
+				Console.WriteLine($"Publish ERROR: {exception.Message}");
+		}
+	}
 }
